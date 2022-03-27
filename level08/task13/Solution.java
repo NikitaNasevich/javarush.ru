e28267abd1dcbb30696349970a60d7d93a28d8db
Nikita Nasevich <nasevich.nik@mail.ru>
2022-03-27 15:19:51 +0300

    Add files via upload

---
+++ level08/task13/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<String>();
+        set.add("Ламыа");
+        set.add("Ламф");
+        set.add("Лвма");
+        set.add("Ламыпаа");
+        set.add("Лпма");
+        set.add("Лрма");
+        set.add("Лома");
+        set.add("Лаема");
+        set.add("Лапа");
+        set.add("Ламыаа");
+        set.add("Ларма");
+        set.add("Ламеа");
+        set.add("Ламка");
+        set.add("Лаома");
+        set.add("Ламиа");
+        set.add("Ламооар");
+        set.add("Ламоа");
+        set.add("Ламрваа");
+        set.add("Ламыра");
+        set.add("Ламак");
+
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
