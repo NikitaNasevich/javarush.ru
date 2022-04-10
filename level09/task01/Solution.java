6a035bea685ad02fb9943bdb3cef2f0cebe326bb
Nikita Nasevich <nasevich.nik@mail.ru>
2022-04-10 20:47:59 +0300

    Add files via upload

---
+++ level09/task01/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task08.task0830;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Задача по алгоритмам Ӏ Java Syntax: 8 уровень, 11 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) {
+        //напишите тут ваш код
+        while (true) {
+            int check = 0;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (isGreaterThan(array[i], array[i + 1])) {
+                    String temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    check++;
+                }
+            }
+            if (check == 0) {
+                break;
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
