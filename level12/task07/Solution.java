f7899f9f4703f3ed845d558cc146383da53794f1
Nikita Nasevich <nasevich.nik@mail.ru>
2022-04-21 17:37:27 +0300

    Add files via upload

---
+++ level12/task07/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 0;
+        print(n);
+        Integer nn = 1;
+        print(nn);
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int n) {
+
+    }
+
+    public static void print(Integer n) {
+
+    }
+}
