d715eac3163788c3db6abd0ca06cabaa62d71255
Nikita Nasevich <nasevich.nik@mail.ru>
2022-04-21 17:39:27 +0300

    Add files via upload

---
+++ level12/task08/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int n) {
+    }
+
+    public static void print(String n) {
+    }
+
+    public static void print(boolean n) {
+    }
+
+    public static void print(char n) {
+    }
+
+    public static void print(byte n) {
+    }
+}
