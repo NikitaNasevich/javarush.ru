29c08d9244dfc3a1cf9179b5f35f74d7f52be379
Nikita Nasevich <nasevich.nik@mail.ru>
2022-04-23 14:47:18 +0300

    Add files via upload

---
+++ level12/task17/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add interfaces here - добавь интерфейсы тут
+    public interface CanFly {
+        static void fle() {
+        }
+    }
+
+    public interface CanRun {
+        static void run() {
+        }
+    }
+
+    public interface CanSwim {
+        static void swim() {
+        }
+    }
+
+}
