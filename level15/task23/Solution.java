8f312038db4b195c0cbba2b95679766c94d63e90
Nikita Nasevich <nasevich.nik@mail.ru>
2022-05-03 15:16:12 +0300

    Add files via upload

---
+++ level15/task23/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    private Solution() {
+    }
+    public Solution(String name){
+
+    }
+    protected Solution(int n){
+    }
+    Solution(String name, int n){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ level15/task23/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(int n) {
+        super(n);
+    }
+
+    SubSolution(String name, int n) {
+        super(name, n);
+    }
+
+    protected SubSolution(String name) {
+        super(name);
+    }
+
+
+}
