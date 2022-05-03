cd2d9b3cc363ac0b2bba4ca662549ea3ad66df07
Nikita Nasevich <nasevich.nik@mail.ru>
2022-05-03 12:19:34 +0300

    Add files via upload

---
+++ level15/task29/Plane.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+
+    private final int numpas;
+
+    public Plane(int numpas) {
+        this.numpas = numpas;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
