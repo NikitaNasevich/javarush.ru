1d499eca76ed65c114c103dbb33c3f446dbba6b8
Nikita Nasevich <nasevich.nik@mail.ru>
2022-06-13 19:08:19 +0300

    Add files via upload

---
+++ level19/task13/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task19.task1913;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Выводим только цифры
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream truePrintStream = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+
+        testString.printSomething();
+
+        String result = change(byteArrayOutputStream.toString());
+
+        System.setOut(truePrintStream);
+
+        System.out.println(result);
+    }
+
+    private static String change(String text) {
+
+        text = text.replaceAll("[^0-9]", "");
+        return text;
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
