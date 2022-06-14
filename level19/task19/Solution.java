f9ac5a4582ee2b8a062b8e33d7f8c1f832e4e25c
Nikita Nasevich <nasevich.nik@mail.ru>
2022-06-14 16:45:33 +0300

    Add files via upload

---
+++ level19/task19/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task19.task1919;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+/* 
+Считаем зарплаты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        TreeMap<String, Double> map = new TreeMap<String, Double>();
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+
+        while (reader.ready()) {
+            String[] line = reader.readLine().split(" ");
+            if (map.containsKey(line[0])) {
+                map.put(line[0], map.get(line[0]) + Double.parseDouble(line[1]));
+            } else {
+                map.put(line[0], Double.parseDouble(line[1]));
+            }
+        }
+
+
+        for (Map.Entry<String, Double> entry: map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        reader.close();
+    }
+}
