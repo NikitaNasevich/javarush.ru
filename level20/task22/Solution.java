736614f2dc0e2e2d9e77714237f75d90dbee5a02
Nikita Nasevich <nasevich.nik@mail.ru>
2022-06-17 18:38:23 +0300

    Add files via upload

---
+++ level20/task22/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task20.task2022;
+
+import java.io.*;
+
+/* 
+Переопределение сериализации в потоке
+*/
+
+public class Solution implements Serializable, AutoCloseable {
+    private transient FileOutputStream stream;
+    private String fileName;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
+    }
+
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\r\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        this.stream = new FileOutputStream(fileName, true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Solution s = new Solution("test");
+        s.writeObject("something");
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test1"));
+        oos.writeObject(s);
+        oos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test1"));
+        Solution s1 = (Solution) ois.readObject();
+        s1.writeObject("something2");
+        ois.close();
+
+
+    }
+}
