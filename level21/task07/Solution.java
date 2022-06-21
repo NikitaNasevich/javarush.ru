e3d4304fd8f7fa6a84191150fa2ebde2115b99bf
Nikita Nasevich <nasevich.nik@mail.ru>
2022-06-21 17:02:08 +0300

    Add files via upload

---
+++ level21/task07/Solution.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/* 
+Глубокое клонирование карты
+*/
+
+public class Solution implements Cloneable {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        clone = solution.clone();
+        System.out.println(solution);
+        System.out.println(clone);
+
+        System.out.println(solution.users);
+        System.out.println(clone.users);
+
+    }
+
+    public Solution clone() {
+        Solution solution = new Solution();
+        Map<String, User> usersClone = new LinkedHashMap();
+        for (Map.Entry<String, User> entry : users.entrySet()) {
+            usersClone.put(entry.getKey(), entry.getValue().clone());
+        }
+        solution.users = usersClone;
+        return solution;
+    }
+
+    protected Map<String, User> users = new LinkedHashMap();
+
+    public static class User implements Cloneable {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return age == user.age && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, name);
+        }
+
+        public User clone() {
+            User newUser = new User(age, name);
+            return newUser;
+        }
+    }
+}
