35ad7c3719b33efc5f118895cfa22e46cfec02aa
Nikita Nasevich <nasevich.nik@mail.ru>
2022-06-22 22:25:28 +0300

    Add files via upload

---
+++ level22/task2207/Solution.java
@@ -0,0 +1,89 @@
+package com.javarush.task.task22.task2207;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Обращенные слова
+*/
+
+public class Solution {
+    public static List<Pair> result = new LinkedList<>();
+
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()))) {
+
+            while (fileReader.ready()) {
+                String line = fileReader.readLine();
+                String[] wordsFromLine = line.split(" ");
+                Collections.addAll(words, wordsFromLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = i + 1; j < words.size(); j++) {
+                StringBuilder builder = new StringBuilder(words.get(i));
+                String check = builder.reverse().toString();
+                if (check.equals(words.get(j))) {
+                    Pair pair = new Pair();
+                    pair.first = words.get(i);
+                    pair.second = words.get(j);
+                    result.add(pair);
+                    words.remove(j);
+                    words.remove(i);
+                    i--;
+                    break;
+
+                }
+            }
+        }
+
+    }
+
+    public static class Pair {
+        String first;
+        String second;
+
+        public Pair() {
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return first == null && second == null ? "" :
+                    first == null ? second :
+                            second == null ? first :
+                                    first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
+
+        }
+    }
+
+}
